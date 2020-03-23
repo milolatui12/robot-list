c27dd2a103e2df1a9e2ddf70237ed7c7601c1408
milo <tranmilo66@gmail.com>
2020-03-23 09:15:58 +0700

    23/03

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -24,22 +25,23 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if(this.state.robots.length === 0){
-            return <h1 className="tc">Loading...</h1>
-        }else {
-            return (
+        return !robots.length ?
+            <h1 className="tc">Loading...</h1> :
+            (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange = {this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
-                </div>    
-            );
-        }   
+                </div>
+            )       
     }  
 }
 
