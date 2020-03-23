10dc93ac8c8cbc450926f7581ca1c8b36b19b2bf
milo <tranmilo66@gmail.com>
2020-03-23 09:09:05 +0700

    Updates

---
+++ precache-manifest.dfff1a14dd331b75f8d42d1fea84e23a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9449f43d138c755ec2cd07f14aef4b48",
+    "url": "/robot-list/index.html"
+  },
+  {
+    "revision": "a430113fac7b0da47aa0",
+    "url": "/robot-list/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2352ada380104816d40b",
+    "url": "/robot-list/static/css/main.a3b3a408.chunk.css"
+  },
+  {
+    "revision": "a430113fac7b0da47aa0",
+    "url": "/robot-list/static/js/2.bcdbe497.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robot-list/static/js/2.bcdbe497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2352ada380104816d40b",
+    "url": "/robot-list/static/js/main.86b0144c.chunk.js"
+  },
+  {
+    "revision": "a6b4ba5d6841fbd4214e",
+    "url": "/robot-list/static/js/runtime-main.2549f732.js"
+  }
+]);
